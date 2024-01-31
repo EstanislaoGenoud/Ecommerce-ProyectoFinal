862e94a298ca78979c99cce03bef5825eb7ff506
Estanislao Genoud <estangeno@gmail.com>
2024-01-31 19:30:40 -0300

    Listo para entregar

---
+++ JS/Cart.js
@@ -0,0 +1,38 @@
+class Cart{
+    constructor(list= []){
+        this.cart=list;
+        
+    }
+
+
+
+    addToCart({id, imageSrc, title, price}){
+        const index=this.cart.findIndex(product =>product.id == id);
+        if(index == -1){
+            console.log('no esta en el carrito');
+        this.cart.push({id, title, imageSrc, price, quantity: 1});
+
+        }else{
+            console.log('ya esta en el carrito e incremento la cantidad');
+            this.cart[index].quantity += 1;
+        }
+
+        localStorage.setItem('Cart', JSON.stringify(this.cart));
+    }
+
+
+    getProducts(){
+        return this.cart;
+    }
+
+    getCount(){
+        const count=this.cart.reduce((cant, product) =>{return cant + product.quantity}, 0)
+        return count;
+    }
+
+    getSum(){
+        const sum=this.cart.reduce((acum, product) =>{return acum + (product.quantity * product.price)}, 0)
+        return sum;
+    }
+
+}
--- JS/dashboard.js
+++ JS/dashboard.js
@@ -2,12 +2,17 @@ const welcomeMessage='Bienvenido'
 const modal= new bootstrap.Modal('#modalCarrito', {});
 const btnModalCarrito= document.querySelector('#btnModalCarrito');
 const cartCount=document.querySelector('#cartCount');
+const cartSum=document.querySelector('#cartSum');
 const listProducts=document.querySelector('#listProducts');
 const modalListProducts=document.querySelector('#modalListProducts');
 const btnClose=document.querySelector('#btnClose');
 const btnGuardar=document.querySelector('#btnGuardar');
 const btnOrder=document.querySelector('#btnOrder');
 const selectCategory=document.querySelector('#selectCategory');
+const listCart= JSON.parse(localStorage.getItem('cart')) || [];
+const cart= new Cart(listCart);
+
+
 
 document.addEventListener('DOMContentLoaded', function(){
     let loginButton=document.getElementById('loginButton');
@@ -21,7 +26,12 @@ document.addEventListener('DOMContentLoaded', function(){
         welcome.style.display="block"
         welcome.innerText=`${welcomeMessage} ${storedUsername}`;
     }
+    
 });
+cartCount.innerText=cart.getCount();
+
+
+
 
 function logout(){
     localStorage.removeItem("username");
@@ -30,12 +40,41 @@ function logout(){
     window.location.href="index.html";
 }
 btnModalCarrito.addEventListener('click', function(){
+    const list=cart.getProducts();
+    renderCart(list);
+
+    cartSum.innerText=cart.getSum();
     modal.show();
+
 });
 
 btnClose.addEventListener('click', ()=>{
     modal.hide();
 });
+
+
+const addToCart = (e) =>{
+    const id= e.target.id;
+    const product= products.find(item => item.id == id);
+    
+    console.table(product);
+    cart.addToCart(product);
+    cartCount.innerText= cart.getCount();
+}
+btnOrder.addEventListener('click',()=>{
+    products.sort((a,b) =>{
+        if(a.price < b.price){
+            return -1
+        }
+        if(a.price > b.price){
+            return 1
+        }
+        return 0
+    })
+    renderProducts(products)
+    btnOrder.setAttribute('disabled', true)
+});
+
 
 const renderProducts=(list)=>{
     listProducts.innerHTML='';
@@ -45,9 +84,27 @@ const renderProducts=(list)=>{
         <h2>${product.title}</h2>
         <img src="${product.imageSrc}" alt="${product.title}">
         <p>$ ${product.price}</p>
-        <button class="btn-per"><i class="fa-solid fa-cart-shopping"></i>Agregar</button>
+        <button id="${product.id}" class="btn-per btnAddCart">Agregar</button>
         </div>`
     });
+    const btns = document.querySelectorAll('.btnAddCart');
+    btns.forEach(btn => {
+        btn.addEventListener('click', addToCart);
+    })
+    
+}
+const renderCart=(list)=>{
+    modalListProducts.innerHTML='';
+    list.forEach(product =>{
+        modalListProducts.innerHTML += //html
+        `<tr>
+            <td><img src="${product.imageSrc}" alt="${product.title}"style="width: 50px;" /></td>
+            <td>${product.title}</td>
+            <td>${product.quantity}</td>
+            <td>${product.price}</td>
+            <td>${product.price * product.quantity}</td>
 
+        </tr>`
+    });
 }
 renderProducts(products);
